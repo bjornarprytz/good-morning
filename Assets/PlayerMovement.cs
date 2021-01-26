319d012a86ddf3457f8641a18668e4834e4f8238
bjornarprytz <bjornarprytz@users.noreply.github.com>
2021-01-26 15:47:19 +0100

    Normalized the house prefab Added gravity and jumping to the player.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,15 +7,43 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float jumpHeight = 1f;
+    public float weight = 10f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.5f;
+    public LayerMask groundMask;
+
+    float gravity = -9.81f;
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+    
+        var isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity * weight);
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime * weight;
+
+        controller.Move(velocity * Time.deltaTime * Time.deltaTime);
+
+
     }
 } 
